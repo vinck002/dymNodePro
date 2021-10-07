021a96d6c39e9cc55b378875a8d59ba3bd29dbcb
Melvin <vinck021@hotmail.com>
2021-10-07 17:58:38 -0400

    any

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -4,8 +4,31 @@ router.get('/notes/notes',(req,res)=>{
     res.send('<p>aqui van las notas </p>');
 })
 
-router.get('/notes/notedb',(req,res)=>{
-    res.send('<p>aqui van las notas </p>');
+router.post('/notes/new-note',(req,res) =>{
+    const {Email_Sub,Pass} = req.body;
+    const error = [];
+    if (!Email_Sub ) {
+        error.push({tet:"please write a Email"})
+    }
+    if (!Pass ) {
+        error.push({tet:"please write a Password"})
+    }
+    if (error.length > 0) {
+        res.render('notes/new-note',{
+            error,
+            Email_Sub,
+            Pass
+        });
+    }else{
+        res.send('Ok')
+    }
+
+    // console.log(Email_Sub,Pass);
+
+});
+
+router.get('/notes/add',(req,res)=>{
+    res.render('notes/new-note');
 })
 
 
